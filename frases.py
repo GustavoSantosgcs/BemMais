19b0ba09f038fe9b6f3e76ac234bdd26a943d54d
GustavoSantosgcs <gustavo.cost.san@hotmail.com>
2025-05-16 11:32:53 -0300

    Adição de mais frases e bordas

--- frases.py
+++ frases.py
@@ -1,16 +1,20 @@
 import random
 
 frases = [
-    
-    "A gentileza é a linguagem que o surdo pode ouvir e o cego pode ver.",
-    "O mundo muda com seu exemplo, não com sua opinião.",
-    "Seja a mudança que você quer ver no mundo.",
-    "Uma atitude positiva é o primeiro passo para o sucesso.",
-    "Cada pequeno ato de bondade conta.",
-    "O bem que você faz hoje será sua força amanhã."
-          
+    "A gentileza é a linguagem que o surdo pode ouvir e o cego pode ver. - Mark Twain",
+    "O mundo muda com seu exemplo, não com sua opinião. - Paulo Freire",
+    "Seja a mudança que você quer ver no mundo. - Mahatma Gandhi",
+    "Uma atitude positiva é o primeiro passo para o sucesso. - Autor desconhecido",
+    "Cada pequeno ato de bondade conta. - Dalai Lama",
+    "O bem que você faz hoje será sua força amanhã. - Autor desconhecido",
+    "A tecnologia move o mundo. - Steve Jobs",
+    "Ética em TI é tão vital quanto código limpo. - Desconhecido"
 ]
 
 def frase_dia():
-     print("\n✨Frase do dia ✨")
-     print(random.choices(frases))
+    frase = random.choice(frases)
+    borda = "=" * (len(frase) + 4)
+    print("\n✨ Frase do Dia ✨")
+    print(borda)
+    print(f"| {frase} |")
+    print(borda)
