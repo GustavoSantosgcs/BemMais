b3e8908bcfee2e3257827f069d213f86795f9649
GustavoSantosgcs <gustavo.cost.san@hotmail.com>
2025-05-31 13:39:33 -0300

    Correção da logica e adição do DocString da função frase_dia

--- frases.py
+++ frases.py
@@ -1,4 +1,7 @@
+import json
+import os
 import random
+import time
 
 frases = [
     "A gentileza é a linguagem que o surdo pode ouvir e o cego pode ver. - Mark Twain",
@@ -11,8 +14,32 @@ frases = [
     "Ética em TI é tão vital quanto código limpo. - Desconhecido"
 ]
 
+arquivo_frase = os.path.join('dados','frase_dia.json')
+
 def frase_dia():
-    frase = random.choice(frases)
+    """
+    Exibe a frase do dia, garantindo que a mesma frase seja mantida até o dia seguinte usando o
+    módulo Time.
+    
+    Verifica também se já existe uma frase armazenada para a data atual no arquivo 'frase_dia.json'.
+    """
+    hoje = time.strftime("%Y-%m-%d")
+
+    # Verificação se já existe uma frase salva para o dia atual
+    if os.path.exists(arquivo_frase):
+        with open(arquivo_frase, 'r') as arquivo:
+            dados = json.load(arquivo)
+        if dados.get("data") == hoje:
+            frase = dados["frase"]
+        else:
+            frase = random.choice(frases)
+            with open(arquivo_frase, 'w') as arquivo:
+                json.dump({"data": hoje, "frase": frase}, arquivo, indent=4)
+    else:
+        frase = random.choice(frases)
+        with open(arquivo_frase, 'w') as arquivo:
+            json.dump({"data": hoje, "frase": frase}, arquivo, indent=4)
+
     borda = "=" * (len(frase) + 4)
     print("\n✨ Frase do Dia ✨")
     print(borda)
