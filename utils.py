9e21755ffda74bb1c326eeb49b6626bf13eb2017
GustavoSantosgcs <gustavo.cost.san@hotmail.com>
2025-07-04 20:40:50 -0300

    Refatora funções utilitárias para a classe Utils, encapsulando as funções nao_vazio e limpar_tela.

--- utils.py
+++ utils.py
@@ -1,15 +1,17 @@
 import os
 
+class Utils:
+    """Funções utilitárias de terminal. """
+    
+    def nao_vazio(mensagem):
+        """Looping até o usuário digitar algo diferente de vazio."""
+        while True:
+            if texto := input(mensagem).strip():   #walrus 
+                return texto.strip()
+            print("Entrada vazia. Tente novamente.\n")
 
-def nao_vazio(mensagem):
-    """Looping até o usuário digitar algo diferente de vazio."""
-    while True:
-        if texto := input(mensagem).strip():   #walrus 
-            return texto.strip()
-        print("Entrada vazia. Tente novamente.\n")
 
-
-def limpar_tela():
-    """Limpa o terminal."""
-    comando = 'cls' if os.name == 'nt' else 'clear'
-    os.system(comando)
+    def limpar_tela():
+        """Limpa o terminal."""
+        comando = 'cls' if os.name == 'nt' else 'clear'
+        os.system(comando)
